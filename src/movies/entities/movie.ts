d836f47cdfd7d15b044c2921d0bd308b563fdf59
deni2004 <112943691+deno2004@users.noreply.github.com>
2025-01-17 12:40:14 +0100

    prvi commit

---
+++ src/movies/entities/movie.ts
@@ -0,0 +1,16 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+    @Entity('movies')
+
+export class Movie {
+    @PrimaryGeneratedColumn
+    id:number;
+    @Column
+    title:string;
+    @Column
+    description:string;
+    @Column
+    release_date:Date;
+    @Column
+    rating:number;
+    }
